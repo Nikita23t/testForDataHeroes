d242f40235f9cbac4a0e3be01853a2db10c72897
Nikita23t <111018784+Nikita23t@users.noreply.github.com>
2024-07-05 13:59:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = {
     "postgres://candidate:62I8anq3cFq5GYh2u4Lh@rc1b-r21uoagjy1t7k77h.mdb.yandexcloud.net:6432/db1",
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync("/home/cat/.postgresql/root.crt").toString(),
+    ca: fs.readFileSync("/root.crt").toString(),
   },
 };
 
